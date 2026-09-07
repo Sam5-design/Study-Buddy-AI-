708e9ea84ee74ce15f8005c05d9ca34a7013f7b1
soumith samuel <soumithkalapala@gmail.com>
2026-09-07 15:29:20 +1000

    Connect Express app to MongoDB via Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+connectDB();
 
 app.set('view engine', 'ejs');
 
