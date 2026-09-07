7cec91c78342d3e380a014e33d45d804a1ecffe6
soumith samuel <soumithkalapala@gmail.com>
2026-09-07 15:32:27 +1000

    Configure Passport.js middleware skeleton with local strategy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('./config/passport');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -7,6 +9,16 @@ const app = express();
 connectDB();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('Study Buddy AI server is running.');
