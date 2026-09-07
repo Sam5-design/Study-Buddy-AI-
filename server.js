0419a61a8aac8cd2ef47c7ca19137f52c52c591f
soumith samuel <soumithkalapala@gmail.com>
2026-09-07 15:15:07 +1000

    Add basic Express server and env example

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Study Buddy AI server is running.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
